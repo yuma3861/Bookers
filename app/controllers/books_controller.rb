76e16530287d63b64035e2c7c18af28a11652a8b
yuma3861 <yuuma0806furu@gmail.com>
2021-08-29 00:27:39 +0000

    [Finish 3-9]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,45 @@
+class BooksController < ApplicationController
+  def index
+      @books = Book.all
+      @book = Book.new
+  end
+
+  def show
+      @book = Book.find(params[:id])
+  end
+
+  def create
+      @book = Book.new(book_params)
+      if @book.save
+      redirect_to book_path(@book), notice: 'Book was successfully created.' #詳細表示へ
+      else
+        @books = Book.all
+        render action: :index
+      end
+  end
+
+  def edit
+      @book = Book.find(params[:id])
+  end
+
+  def update
+      @book = Book.find(params[:id])
+      if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully updated.' #詳細画面へ
+      else
+        @books = Book.all
+        render action: :edit
+      end
+  end
+
+  def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path, notice: 'Book was successfully destroyed.'
+  end
+
+  private #
+  def book_params
+      params.require(:book).permit(:title, :body)
+  end
+end
